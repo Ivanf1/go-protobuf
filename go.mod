3a68a0c50af9a57419fd555d7132216cf29228aa
Ivan <capobianco.ivan@gmail.com>
2022-06-27 23:28:56 +0200

    build: use httprouter

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Ivanf1/go-protobuf
 go 1.18
 
 require google.golang.org/protobuf v1.28.0
+
+require github.com/julienschmidt/httprouter v1.3.0
--- src/main.go
+++ src/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	// pb "github.com/Ivanf1/go-protobuf/protos"
+
+	pb "github.com/Ivanf1/go-protobuf/protos"
+
+	"github.com/julienschmidt/httprouter"
+	"google.golang.org/protobuf/proto"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	})
+func DeviceMessageHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	device := pb.Device{}
 
-	http.ListenAndServe(":3000", nil)
+	data, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if err := proto.Unmarshal(data, &device); err != nil {
+		fmt.Println(err)
+	}
+
+	println(device.Id, ":", device.Name)
+}
+
+func main() {
+	router := httprouter.New()
+	router.POST("/", DeviceMessageHandler)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
